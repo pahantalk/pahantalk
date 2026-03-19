fdba01cdf12e4606c27e46ef6a495478f45d0df5
pahantalk <gosha289977@gmail.com>
2026-03-19 03:30:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -676,4 +676,5 @@ def get_messages(username):
     return jsonify(msgs)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
