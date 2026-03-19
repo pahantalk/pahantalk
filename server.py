90e19e1cd07be4cb5ab33ad4086d3c298e29aa42
pahantalk <gosha289977@gmail.com>
2026-03-20 02:28:49 +0300

    Update server.py

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ init_db()
 HTML = '''<!DOCTYPE html>
 <html>
 <head>
-    <title>ПАХАНТАЛК — Telegram Style</title>
+    <title>ПАХАНТАЛК</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
